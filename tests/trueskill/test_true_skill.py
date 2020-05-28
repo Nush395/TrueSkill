73020fd665d30bb7ad413134ab4bd132e10cc908
Nush395 <anushan395@hotmail.co.uk>
2020-05-28 17:18:28 +0100

    Fix broken test for agreement between analytic and factor graph computation of team skill updates. Added test to check three team case converges.

--- tests/trueskill/test_true_skill.py
+++ tests/trueskill/test_true_skill.py
@@ -30,39 +30,48 @@ class TestTrueSkill(unittest.TestCase):
         self.assertAlmostEqual(new_ratings["loser"].sigma,
                                updated_loser.sigma, 2)
 
-    @unittest.skip("Current skill updates are consistent in direction and"
-                   "size but don't agree exactly.")
     def test_factor_graph_agrees_with_two_team_explicit(self):
         # given
         p1 = Gaussian(mu=MU, sigma=SIGMA)
         p2 = Gaussian(mu=MU, sigma=SIGMA)
         p3 = Gaussian(mu=MU, sigma=SIGMA)
-        losing_team = {"player1": p1, "player2": p2}
-        winning_team = {"player3": p3}
+        losing_team = {"player3": p3, "player2": p2}
+        winning_team = {"player1": p1}
         teams = [winning_team] + [losing_team]
 
         # when
-        # we update skills with the analytic method
+        # we update skills with the factor graph
+        ts = TrueSkill(teams)
+        new_ratings = ts.update_ratings()
+        # and update skills with the analytic method
         update_ratings_in_team(winning_team, losing_team)
-        # and update skills with the factor graph
+
+        # then
+        self.assertAlmostEqual(new_ratings["player1"].mu,
+                               winning_team["player1"].mu, 2)
+        self.assertAlmostEqual(new_ratings["player2"].mu,
+                               losing_team["player2"].mu, 2)
+        self.assertAlmostEqual(new_ratings["player3"].mu,
+                               losing_team["player3"].mu, 2)
+        self.assertAlmostEqual(new_ratings["player1"].sigma,
+                               winning_team["player1"].sigma, 2)
+        self.assertAlmostEqual(new_ratings["player2"].sigma,
+                               new_ratings["player2"].sigma, 2)
+        self.assertAlmostEqual(new_ratings["player3"].sigma,
+                               losing_team["player3"].sigma, 2)
+
+    def test_three_team_converges(self):
+        # given
+        p1 = Gaussian(mu=MU, sigma=SIGMA)
+        p2 = Gaussian(mu=MU, sigma=SIGMA)
+        p3 = Gaussian(mu=MU, sigma=SIGMA)
+        teams = [{"player1": p1}, {"player2": p2}, {"player3": p3}]
+
+        # when
         ts = TrueSkill(teams)
         new_ratings = ts.update_ratings()
 
-        # then
-        print(new_ratings["player1"])
-        print(losing_team["player1"])
-        self.assertAlmostEqual(new_ratings["player1"].mu,
-                               losing_team["player1"].mu)
-        self.assertAlmostEqual(new_ratings["player2"].mu,
-                               losing_team["player2"].mu)
-        self.assertAlmostEqual(new_ratings["player3"].mu,
-                               winning_team["player3"].mu)
-        self.assertAlmostEqual(new_ratings["player1"].sigma,
-                               losing_team["player1"].sigma)
-        self.assertAlmostEqual(new_ratings["player2"].sigma,
-                               new_ratings["player2"].sigma)
-        self.assertAlmostEqual(new_ratings["player3"].sigma,
-                               winning_team["player3"].sigma)
+        # then expect a convergence
 
 
 
